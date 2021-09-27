ddaa58f614b295106a2ae1d78369f765cfb38d8f
efoejean <efoejean@gmail.com>
2021-09-26 23:58:59 -0500

    Calculate the total balance

--- app/index.js
+++ app/index.js
@@ -3,4 +3,13 @@ const nimBalances = infos
   .map(({ Accounts }) => Accounts)
   .filter(({ balance }) => balance >= 5000)
   .flat();
-console.log(nimBalances);
+// console.log(nimBalances);
+
+const totalBalances = infos.reduce((total, person) => {
+  total += person.Accounts.reduce((accountTotal, { balance }) => {
+    accountTotal += Number(balance);
+    return accountTotal;
+  }, 0);
+  return total;
+}, 0);
+console.log(totalBalances);
