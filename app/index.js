4d8bb9ccef60c564671c864584997c350e0d1576
efoejean <efoejean@gmail.com>
2021-09-26 23:39:54 -0500

    Filter account balance that >= 5000

--- app/index.js
+++ app/index.js
@@ -1,25 +1,6 @@
-import faker from "faker";
-import { promises as fs } from "fs";
-
-const data = Array.from({ length: 25 }, (_, i) => ({
-  id: i + 1,
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  phone: faker.phone.phoneNumber(),
-  avatar: faker.image.avatar(),
-  Accounts: Array.from(
-    { length: faker.datatype.number({ min: 1, max: 4 }) },
-    (_, i) => ({
-      id: i + 1,
-      accountName: faker.finance.accountName(),
-      routingNumber: faker.finance.routingNumber(),
-      currency: faker.finance.currencyCode(),
-      balance: faker.finance.amount(1, 10000, 2),
-    })
-  ),
-}));
-
-fs.writeFile(
-  "./app/data.js",
-  `export default ${JSON.stringify(data, null, 2)}`
-);
+import infos from "./data.js";
+const nimBalances = infos
+  .map(({ Accounts }) => Accounts)
+  .filter(({ balance }) => balance >= 5000)
+  .flat();
+console.log(nimBalances);
