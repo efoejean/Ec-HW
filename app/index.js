0406617d3137db88f059b0fbb15dcd8e36929969
efoejean <efoejean@gmail.com>
2021-09-26 23:16:28 -0500

    reduce the length of the array.

--- app/index.js
+++ app/index.js
@@ -1,18 +1,18 @@
 import faker from "faker";
 import { promises as fs } from "fs";
 
-const data = Array.from({ length: 50 }, (_, i) => ({
+const data = Array.from({ length: 25 }, (_, i) => ({
   id: i + 1,
   name: faker.name.findName(),
   email: faker.internet.email(),
   phone: faker.phone.phoneNumber(),
   avatar: faker.image.avatar(),
   Accounts: Array.from(
-    { length: faker.datatype.number({ min: 1, max: 5 }) },
+    { length: faker.datatype.number({ min: 1, max: 4 }) },
     (_, i) => ({
       id: i + 1,
-      companyName: faker.company.companyName(),
       accountName: faker.finance.accountName(),
+      routingNumber: faker.finance.routingNumber(),
       currency: faker.finance.currencyCode(),
       balance: faker.finance.amount(1, 10000, 2),
     })
